6db047b51634f6577a8b2866506f4cb18cdb9f11
SRINITHI2024 <727722EUEC181@skcet.ac.in>
2024-08-05 10:11:06 +0530

    tic

---
+++ java.js
@@ -0,0 +1,11 @@
+var vote = () =>{
+    age=18
+
+if(age>=18){
+    console.log(" do vote");
+}
+else{
+    console.log("dont vote");
+}
+}
+vote()
---
+++ practice.js
@@ -0,0 +1,17 @@
+arr=[10,20,true];
+/*arr.forEach((ind)=>{
+    console.log(ind);
+});*/
+
+/*for(i of arr){
+    console.log(i)
+}*/
+
+/*for(i in arr){
+    console.log(i);
+}*/
+
+//destructing operator
+/*var[a,b,c]=arr
+console.log(arr,a,b,c,);/*
+
